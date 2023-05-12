9c5dcb13f531d0923668518d5c38d88f0fed1592
Vision Filler <visionfiller@gmail.com>
2023-05-12 14:27:36 -0500

    feed and string

--- walking/alpaca.py
+++ walking/alpaca.py
@@ -3,7 +3,7 @@ from datetime import date
 
 class Alpaca:
 
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
@@ -11,3 +11,8 @@ class Alpaca:
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
+        self.food = food
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
